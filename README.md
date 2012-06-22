b4aaa7f3f47d6edaca3c10785a5b78e7004667b4
Adler Medrado <adlermedrado@gmail.com>
2012-06-22 19:07:21 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+**Simple script to show my coderwall badges**
+
+Made to be simple:
+
+Include  scripts/my_coderwall.js at your HTML file:
+	
+	<script src="scripts/my_coderwall.js" type="text/javascript" charset="utf-8"></script>
+	
+Create a place to put your (section, div, whatever) badges:
+
+	<section id="coderwall_badges"></section>
+	
+Create an instance of MyCoderWallBadges object passing the object's name and your coderwall's username:
+
+	<script type="text/javascript" charset="utf-8">
+	new myCoderwallBadges('coderwall_badges', 'adlermedrado');
+	</script>
+	
+** jQuery is required **
---
+++ scripts/my_coderwall.js
@@ -0,0 +1,18 @@
+function myCoderwallBadges(objName, username) {
+	this.objName = objName;
+	this.username = username;
+	var currentObject = this;
+	
+	getMyBadges = function(param) {
+		for (i in param.data.badges) {
+			var currentBadge = param.data.badges[i];
+			$('#'+currentObject.objName).append(
+			   $('<img>').attr('src',currentBadge.badge)
+			   			 .attr('title',currentBadge.name+": "+currentBadge.description)
+			   			 .attr('alt',currentBadge.name+": "+currentBadge.description)						 
+			);
+		}		
+	}
+	
+	$.getScript("http://coderwall.com/"+this.username+".json?callback=this.getMyBadges");
+}
